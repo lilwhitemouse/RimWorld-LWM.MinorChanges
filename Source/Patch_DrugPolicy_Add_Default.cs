cf1d659702e30506b1ac88b8aa9ec55c916a790e
LWM <lilmouse@littlewhitemouse.net>
2020-01-07 11:29:59 -0500

    Penoxycyline defaults to every 5 days

---
+++ Source/Patch_DrugPolicy_Add_Default.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
+using System.Reflection.Emit; // for OpCodes in Harmony Transpiler
+using Harmony;
+
+/*******************************************
+ * When a new drug policy is created, make *
+ * penoxycyline default to every 5 days.   *
+ *******************************************/
+
+namespace LWM.MinorChanges
+{
+    /************* DrugPolicy's InitializeIfNeeded *************/
+    // This seems the obvious place to patch.
+    // Add a check to add defaults to DrugPolicyEntries:
+    //   DrugPolicyEntry drugPolicyEntry = new DrugPolicyEntry();
+    //   drugPolicyEntry.drug = allDefsListForReading[i];
+    //   drugPolicyEntry.allowedForAddiction = true;
+    // * AddDefaults(drugPolicyEntry); //   Add this line into the code!
+    //   this.entriesInt.Add(drugPolicyEntry);
+    //
+    // It's easy enough to insert a Transpiler command here, so we do it.
+    [HarmonyPatch(typeof(RimWorld.DrugPolicy), "InitializeIfNeeded")]
+    public static class Patch_DrugPolicy_IIN {
+        public static bool Prepare() {
+            return LoadedModManager.GetMod<MinorChangesMod>().GetSettings<Settings>().applyDrugDefaults;
+        }
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+                                                       ILGenerator generator) {
+            List<CodeInstruction> code=instructions.ToList();
+            for (int i=0; i<code.Count; i++) {
+                yield return code[i];
+                if (code[i].opcode==OpCodes.Stfld &&
+                    (code[i].operand as FieldInfo).Name=="allowedForAddiction") {
+                    yield return new CodeInstruction(OpCodes.Ldloc_2);
+                    yield return new CodeInstruction(OpCodes.Call,
+                                                     typeof(Patch_DrugPolicy_IIN)
+                                                     .GetMethod("AddDefaults", BindingFlags.NonPublic
+                                                         |BindingFlags.Static));
+                }
+            }
+        }
+        static void AddDefaults(DrugPolicyEntry dp) {
+            // I hard code these values here.  I'm sure there's a slicker way to do this using XML etc.
+            // But this is way easier and it does what *I* need, at any rate.
+            if (dp.drug.defName=="Penoxycyline") {
+                dp.daysFrequency=5f;
+            }
+        }
+    }
+}
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -13,16 +13,50 @@ namespace LWM.MinorChanges
 {
     public class Settings : ModSettings
     {
-
-        public void DoSettingsWindowContents(Rect inRect) {
-
-
-        }
-
         public override void ExposeData() {
             Scribe_Values.Look(ref smelterIsHot, "smelterIsHot", true);
             Scribe_Values.Look(ref bigComputersAreHot, "bigComputersAreHot", true);
+
+            Scribe_Values.Look(ref applyDrugDefaults, "applyDrugDefaults", true);
         }
+
+        public void DoSettingsWindowContents(Rect inRect) {
+            Rect rectWeCanSee=inRect.ContractedBy(10f);
+            rectWeCanSee.height-=100f; // "close" button
+            bool scrollBarVisible = totalContentHeight > rectWeCanSee.height;
+            Rect rectThatHasEverything=new Rect(0f,0f,rectWeCanSee.width-
+                                                (scrollBarVisible ? ScrollBarWidthMargin : 0),totalContentHeight);
+            Widgets.BeginScrollView(rectWeCanSee, ref scrollPosition, rectThatHasEverything);
+            float curY=0f;
+            Rect r=new Rect(0,curY,rectThatHasEverything.width, LabelHeight);
+            Widgets.Label(r, "IMPORTANT REMINDER: you MUST restart the game for changes to take effect");
+
+            r=new Rect(0,curY,rectThatHasEverything.width, LabelHeight);
+            Widgets.CheckboxLabeled(r, "Smelter gives off heat", ref smelterIsHot); // TODO: translate
+            TooltipHandler.TipRegion(r, "It takes a LOT of heat to smelt steel, and insulation on the Rim is lacking...");
+            curY+=LabelHeight+1f;
+
+            r=new Rect(0,curY,rectThatHasEverything.width, LabelHeight);
+            Widgets.CheckboxLabeled(r, "Computers get hot too", ref bigComputersAreHot); // TODO: translate
+            TooltipHandler.TipRegion(r, "Server rooms need giant air conditioning systems.  Are computers on the Rim somehow super efficient?  Ha.  High Tech Research Benches produce heat.");
+            curY+=LabelHeight+1f;
+
+            r=new Rect(0,curY,rectThatHasEverything.width, LabelHeight);
+            Widgets.CheckboxLabeled(r, "Penoxycyline defaults to 'take every 5 days'", ref applyDrugDefaults); // TODO: translate
+            TooltipHandler.TipRegion(r, "If there are other drugs that should have this sort of default, let me know?");
+            curY+=LabelHeight+1f;
+
+
+            Widgets.EndScrollView();
+            totalContentHeight=curY+50f;
+        }
+        private static Vector2 scrollPosition=new Vector2(0f,0f);
+        private static float totalContentHeight=1000f;
+        private const float TopAreaHeight = 40f;
+        private const float TopButtonHeight = 35f;
+        private const float TopButtonWidth = 150f;
+        private const float ScrollBarWidthMargin = 18f;
+        private const float LabelHeight=22f;
 
         // Grab a given setting given its string name:
         //   (only allow boolean results, eh?)
@@ -44,5 +78,7 @@ namespace LWM.MinorChanges
 
         bool smelterIsHot=true;
         bool bigComputersAreHot=true;
+
+        public bool applyDrugDefaults=true;
     }
 }
