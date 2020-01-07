9ae89eff07613163e472106d7cb223a58d8038e6
LWM <lilmouse@littlewhitemouse.net>
2020-01-07 13:07:00 -0500

    Better Spots

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -18,6 +18,8 @@ namespace LWM.MinorChanges
             Scribe_Values.Look(ref bigComputersAreHot, "bigComputersAreHot", true);
 
             Scribe_Values.Look(ref applyDrugDefaults, "applyDrugDefaults", true);
+
+            Scribe_Values.Look(ref betterSpots, "betterSpots", true);
         }
 
         public void DoSettingsWindowContents(Rect inRect) {
@@ -46,6 +48,10 @@ namespace LWM.MinorChanges
             TooltipHandler.TipRegion(r, "If there are other drugs that should have this sort of default, let me know?");
             curY+=LabelHeight+1f;
 
+            r=new Rect(0,curY,rectThatHasEverything.width, LabelHeight);
+            Widgets.CheckboxLabeled(r, "Better 'Spots'", ref betterSpots); // TODO: translate
+            TooltipHandler.TipRegion(r, "Lets you place spots pretty much anywhere.\nWant to get married in a cornfield?  Sure.  In the river?  Sure.  In the mud?  Sure.  In the lava?  Sure....okay, if you pawns cannot get there, then no.  This includes inside walls.\nSpots affected:\n  Marriage Spot\n  Party Spot\n  Caravan Packing Spot\n  Trading Spot (any mod that uses the defName TradingSpot)");
+            curY+=LabelHeight+1f;
 
             Widgets.EndScrollView();
             totalContentHeight=curY+50f;
@@ -80,5 +86,7 @@ namespace LWM.MinorChanges
         bool bigComputersAreHot=true;
 
         public bool applyDrugDefaults=true;
+
+        bool betterSpots=true;
     }
 }
