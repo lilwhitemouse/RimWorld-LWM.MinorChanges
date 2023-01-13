933cbd975a057d294200476c2125ceed9453617c
LWM <lilmouse@littlewhitemouse.net>
2023-01-13 02:46:06 -0500

    Because sometimes it's useful

---
+++ Source/Debugging.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
+using System.Reflection.Emit; // for OpCodes in Harmony Transpiler
+using HarmonyLib;
+#if DEBUG
+namespace LWM.MinorChanges
+{
+
+}
+#endif
