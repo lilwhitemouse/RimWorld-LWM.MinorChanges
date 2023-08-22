95195b1c9b9f7894f2b63b4dac0894ae35fecafa
LWM <lilmouse@littlewhitemouse.net>
2023-08-22 11:40:55 -0400

    Better naming, better settings

--- Source/Patch_Verb_CastAbility-TwoThings.cs
+++ Source/Patch_Verb_CastAbility-TwoThings.cs
@@ -7,8 +7,7 @@ using HarmonyLib;
 
 namespace LWM.MinorChanges
 {
-    //TODO: Find what lets someone's grey path get reset whenyou specify eating something so can 
-    //  duplicate for our job
+    /******************NOTE: Both ValidateTarget and CanHitTarget are patched **********/
     /***********************************
      * Verb_CastAbility's ValidateTarget won't let anything ranged attempt to test for a valid target
      *   if it's outside of immediate line of sight or current range.  We patch range to zero for the 
@@ -19,16 +18,22 @@ namespace LWM.MinorChanges
     {
         public static bool Prepare()
         {
-            // TODO: If we should want to add more affected Abilities, THIS is the place to set up
-            //   ShouldPatchesAffectThisVerb
-            return LoadedModManager.GetMod<MinorChangesMod>().GetSettings<Settings>().betterSolarPinholes;
+            return Settings.IsOptionSet("easierCasting");
         }
 
         public static bool ShouldPatchesAffectThisVerb(Verb_CastAbility v)
         {
-            if (v.ability.def.defName == "SolarPinhole")
+            // We check to see if it's OUR JobDriver:
+            // NOTE: the static constructor for our jobdriver contains the code that applies 
+            //   the jobdriver to any abilities that should be applied (e.g., Solar Pinhole, Pain Block, &c)
+            return v.ability.def.jobDef == JobDriver_CastAbilityGoTo_Distance.jobDef;
+            /*
+            if (v.ability.def.jobDef == JobDriver_CastAbilityGoTo_Distance.jobDef)
+            {
                 return true;
+            }
             return false;
+            */           
         }
 
         static bool inOurValidation = false;
@@ -51,13 +56,14 @@ namespace LWM.MinorChanges
      * Current test for Verb_CastAbility's CanHitTarget is
      *     return this.verbProps.range <= 0f || base.CanHitTarget(targ);
      *   We are pretending range is 0f, so we'll just short circuit test immediately
+     *   and avoid any questions of trying to hit a target through a wall & far away
      */
     [HarmonyPatch(typeof(Verb_CastAbility), "CanHitTarget")]
     static class Patch_Verb_CastAbility_CanHitTarget
     {
         public static bool Prepare()
         {
-            return LoadedModManager.GetMod<MinorChangesMod>().GetSettings<Settings>().betterSolarPinholes;
+            return Settings.IsOptionSet("easierCasting");
         }
 
         static bool Prefix(Verb_CastAbility __instance, ref bool __result)
